d88075a94fef5207dbf28d7677494492ad2d1cbe
Pierre Rudloff <contact@rudloff.pro>
2015-11-22 02:23:19 +0100

    Cleanup footer

--- htdocs/content/themes/rudloff/app/views/footer.scout.php
+++ htdocs/content/themes/rudloff/app/views/footer.scout.php
@@ -1,6 +1,6 @@
 	<footer class="footer clearfix small">
 		<p class="colophon">
-			&copy; <span itemprop="brand">Pierre Rudloff</span> 2013 &mdash; Siret <span itemprop="taxID">75108721400027</span> &mdash; Graphisme d'<a href="http://olivierhaquette.fr/" itemprop="colleague">Olivier Haquette</a>
+			Graphisme d'<a target="_blank" href="http://olivierhaquette.fr/">Olivier Haquette</a>
 		</p>
 	</footer>
 	{{ wp_footer() }}
